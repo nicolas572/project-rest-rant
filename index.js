23fc14d40da7ef5bbc7d6464933e720e9aef3f09
Nicolas Nueca <nicolasnueca@Nicolass-MacBook-Air.local>
2023-05-27 09:31:30 -0700

    added 404 page

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ app.get('/', function (req, res){
     res.send('Hello World')
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
 app.listen(process.env.PORT)
