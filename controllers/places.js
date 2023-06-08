eb2ff407fc1a4a935270ab43729b66d667a31433
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-06-07 22:21:37 -0700

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -36,6 +36,21 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
+
   
 
 module.exports = router
