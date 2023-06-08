3cc80da9141118bf173e753fcf4648d562d9c03b
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-06-07 22:08:39 -0700

    creating edit and delete buttons

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -10,6 +10,14 @@ function show (data) {
             <p>Currently unrated</p>
             <h3>Comments</h3>
             <p>No comments yet!</p>
+            <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+              Edit
+            </a>
+            <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+              <button type='submit' className='btn btn-danger'>
+                Delete
+              </button>
+            </form>
           </main>
         </Def>
     )
