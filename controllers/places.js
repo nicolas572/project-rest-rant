59d25206354b0c45630caacb0927c96fde8688c0
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-06-20 23:05:03 -0700

    implementing comment form

--- controllers/places.js
+++ controllers/places.js
@@ -28,9 +28,12 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+//Show
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+    console.log(place.comments)
     res.render('places/show', {place})
   })
   .catch(err => {
@@ -50,6 +53,28 @@ router.delete('/:id', (req, res) => {
 router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,7 +1,27 @@
 const React = require('react')
 const Def = require('../default')
+const comments = require('../../models/comment')
 
 function show (data) {
+    let comments= (
+        <h3 className='inactive'>
+            No Comments Yet!
+        </h3>
+    )
+    if(data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
           <main>
@@ -34,9 +54,36 @@ function show (data) {
                     <h2>
                         Comments
                     </h2>
-                    <p>No Comments Yet!</p>
+                    {comments}
                 </div>
             </div>
+            <div className="reviewForm">
+            <h2>Is {data.place.name} a place to Rave 💖 or a Rant 🤬 about?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Your Name</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">⭐️ Star Rating</label>
+                  <input type="range" step="1" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="col-sm-2">
+                  <label className="form-check-label" htmlFor="rant" id="rant-checkbox">Rant?</label>
+                  <br/>
+                  <input type="checkbox" id="rant" name="rant" className="form-check-input" />
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
+            </div>  
             <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
             <i className="bi bi-pencil"></i> Edit
             </a>
